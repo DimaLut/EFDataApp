5564f4558459ff690772e85a11efb00197ec2eda
DimaLut <69689216+DimaLut@users.noreply.github.com>
2021-05-05 21:42:56 +0300

    Добавьте файлы проекта.

---
+++ EFDataApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EFDataApp.Models; // пространство имен моделей и контекста данных
+
+namespace EFDataApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
